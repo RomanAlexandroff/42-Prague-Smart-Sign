9f6759e0a1ae58643f62d023ebffd66a2b518bb3
RomanAlexandroff <r.aleksandroff@gmail.com>
2024-02-29 19:12:35 +0100

    add OTA keep connection alive loop

--- ota_mode.cpp
+++ ota_mode.cpp
@@ -1,7 +1,22 @@
 
 #include "42-Prague-Smart-Sign.h"
 
-void  ft_ota_init(String chat_id) 
+static void  ft_ota_waiting_loop(void)
+{
+    int i;
+
+    i = 0;
+    DEBUG_PRINTF("\nThe OTA Waiting Loop is running...\n", "");
+    while (i < WAIT_FOR_OTA_LIMIT)
+    {
+        ArduinoOTA.handle();
+        ft_delay(500);
+        i++;
+    }
+    DEBUG_PRINTF("\nThe OTA Waiting Loop exited without an update: TIME OUT\n", "");
+}
+
+void  ft_ota_init(void)
 {
     const char* ssid;
     uint16_t    maxlen;
@@ -17,17 +32,15 @@ void  ft_ota_init(String chat_id)
     snprintf(fullhostname, maxlen, "%s-%02x%02x%02x", nameprefix, mac[3], mac[4], mac[5]);
     ArduinoOTA.setHostname(fullhostname);
     delete[] fullhostname;
-    ArduinoOTA.onStart([chat_id]() {
+    ArduinoOTA.onStart([]() {
         String type;
         if (ArduinoOTA.getCommand() == U_FLASH)
             type = "sketch";
         else
             type = "filesystem";
-        bot.sendMessage(chat_id, "Updating...", "");
         ft_display_bitmap(badge_bitmap_ota_updating);
     });
-    ArduinoOTA.onEnd([chat_id]() {
-        bot.sendMessage(chat_id, "Successfully updated!", "");
+    ArduinoOTA.onEnd([]() {
         ft_display_bitmap(badge_bitmap_ota_success);
         ft_delay(5000);
         ft_clear_display(true);
@@ -35,13 +48,13 @@ void  ft_ota_init(String chat_id)
     });
     ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
     });
-    ArduinoOTA.onError([chat_id](ota_error_t error) {
-        bot.sendMessage(chat_id, "Something went wrong. Updating was not completed. Try again", "");
+    ArduinoOTA.onError([](ota_error_t error) {
         ft_display_bitmap(badge_bitmap_ota_fail);
         ft_delay(3000);
         ft_clear_display(true);
         ESP.restart();
     });
     ArduinoOTA.begin();
+    ft_ota_waiting_loop();
 }
  
