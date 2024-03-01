f6ed03d83a730ba40f1a67aec260a03360dbc9a5
RomanAlexandroff <r.aleksandroff@gmail.com>
2024-03-01 21:03:31 +0100

    add file for data fetch from Intra functions

--- Intra_interaction.cpp
+++ Intra_interaction.cpp
@@ -1,6 +1,24 @@
 
 #include "42-Prague-Smart-Sign.h"
 
+void  ft_fetch_data(void)
+{
+    if (WiFi.status() != WL_CONNECTED)
+        WiFi.reconnect();
+    if (WiFi.status() != WL_CONNECTED)
+    {
+        DEBUG_PRINTF("Failed to obtain Intra data due to Wi-Fi connection issues\n", "");
+        WiFi.disconnect(true);
+        WiFi.mode(WIFI_OFF);
+        return;
+    }
+    if (!client.connect("https://api.intra.42.fr", 442))
+    {
+        DEBUG_PRINT("Intra server connection FAILED");
+        return;
+    }
 
+   
+}
 
  
