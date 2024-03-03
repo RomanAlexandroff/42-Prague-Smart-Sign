a5ccd1965e7aec13815cdae51a08763a4d6aa806
RomanAlexandroff <r.aleksandroff@gmail.com>
2024-03-03 19:35:31 +0100

    refactor Intra functions

--- Intra_interaction.cpp
+++ Intra_interaction.cpp
@@ -1,27 +1,53 @@
 
 #include "42-Prague-Smart-Sign.h"
 
-void  ft_fetch_exams(void)
+static void  ft_clean_data(String line, String* p_exam_begin, String* p_exam_end)
 {
-    String        data;
-    String        line;
-    const String  token;
+    while (client.available())
+    {
+        line = client.readStringUntil('\n');
+        if (line.startsWith("\"begin_at\":\"")) 
+        {
+            *p_exam_begin = line.substring(24, 28);
+            DEBUG_PRINTF("Exam starts at: " + *p_exam_begin);
+        }
+                if (line.startsWith("\"end_at\":\"")) 
+        {
+            *p_exam_end = line.substring(24, 28);
+            DEBUG_PRINTF("Exam ends at: " + *p_exam_end);
+            break;
+        }
+    }
+}
 
-    if (WiFi.status() != WL_CONNECTED)
-        WiFi.reconnect();
-    if (WiFi.status() != WL_CONNECTED)
+static void  ft_get_data_load(const String token, String* p_line)
+{
+    String  query;
+
+    query = "/exams?filter[location]=" + CLUSTER_ID + "&range[begin_at]=" + 
+            rtc_g.year + "-" + rtc_g.month + "-" + rtc_g.day + "T05:00:00.000Z," +
+            rtc_g.year + "-" + rtc_g.month + "-" + rtc_g.day + "T22:00:00.000Z";
+    client.print(String("GET /v2/campus/" + CAMPUS_ID + query + " HTTP/1.1\r\n" +
+               "Host: api.intra.42.fr\r\n" +
+               "Authorization: Bearer " + token + "\r\n" +
+               "Connection: close\r\n\r\n"));
+    DEBUG_PRINTF("Request for Exam Data sent\n", "");
+    while (client.connected())
     {
-        DEBUG_PRINTF("Failed to obtain Intra data due to Wi-Fi connection issues\n", "");
-        WiFi.disconnect(true);
-        WiFi.mode(WIFI_OFF);
-        return;
+        *p_line = client.readStringUntil('\n');
+        if (*p_line == "\r")
+        {
+            DEBUG_PRINTF("Headers received\n", "");
+            break;
+        }
     }
-    WiFiClientSecure client;
-    if (!client.connect("https://api.intra.42.fr", 443))
-    {
-        DEBUG_PRINTF("Intra server connection FAILED\n", "");
-        return;
-    }
+}
+
+static void  ft_get_token(const String* p_token)
+{
+    String  data;
+    String  line;
+
     data = "grant_type=client_credentials&client_id=" + UID + "&client_secret=" + SECRET;
     client.print(String("POST /oauth/token HTTP/1.1\r\n" +
                "Host: api.intra.42.fr\r\n" +
@@ -43,16 +69,38 @@ void  ft_fetch_exams(void)
         line = client.readStringUntil('\n');
         if (line.startsWith("{\"access_token\":\"")) 
         {
-            token = line.substring(17, line.length() - 3);
-            DEBUG_PRINTF("Access Token: " + token);
+            *p_token = line.substring(17, 80);
+            DEBUG_PRINTF("Access Token: " + *p_token);
             break;
         }
     }
-    client.print(String("GET /v2/campus/56/exams HTTP/1.1\r\n" +
-               "Host: api.intra.42.fr\r\n" +
-               "Authorization: Bearer " + token + "\r\n" +
-               "Connection: close\r\n\r\n"));
-    DEBUG_PRINTF("Request for Exam Data sent\n", "");
+}
+
+void  ft_fetch_exams(void)
+{
+    String        line;
+    const String  token;
+    String        exam_begin;
+    String        exam_end;
+
+    if (WiFi.status() != WL_CONNECTED)
+        WiFi.reconnect();
+    if (WiFi.status() != WL_CONNECTED)
+    {
+        DEBUG_PRINTF("Failed to obtain Intra data due to Wi-Fi connection issues\n", "");
+        WiFi.disconnect(true);
+        WiFi.mode(WIFI_OFF);
+        return;
+    }
+    WiFiClientSecure client;
+    if (!client.connect("https://api.intra.42.fr", 443))
+    {
+        DEBUG_PRINTF("Intra server connection FAILED\n", "");
+        return;
+    }
+    ft_get_token(&token);
+    ft_get_data_load(token, &line);
+    ft_clean_data(line, &exam_begin, &exam_end);
     client.stop();
 }
  
