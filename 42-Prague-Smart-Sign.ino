5411e0389acb79d1677e6316118a67eba0defa5a
RomanAlexandroff <r.aleksandroff@gmail.com>
2024-03-04 23:17:28 +0100

    add comments for easier development and documentation

--- 42-Prague-Smart-Sign.ino
+++ 42-Prague-Smart-Sign.ino
@@ -8,22 +8,22 @@ static void  setup(void)
         ft_delay(50);
         DEBUG_PRINTF("\n\nDEVICE START\nversion %f\n", float(SOFTWARE_VERSION));
     #endif
-    ft_eeprom_init();           // всегда
-    ft_battery_init();          // всегда
-    ft_display_init();          // всегда
-    ft_power_down_recovery();   // всегда
-    ft_battery_check();         // всегда
-    ft_buttons_init();          // всегда
+    ft_eeprom_init();                               // активизируем ячейку памяти для данных состояния аккумулятора
+    ft_battery_init();                              // активируем измерение заряда аккумулятора
+    ft_display_init();                              // активируем связь с дисплеем
+    ft_power_down_recovery();                       // проверяем от чего проснулись и реагируем
+    ft_battery_check();                             // проверяем актуальный заряд аккумулятора
+    ft_buttons_init();                              // активируем кнопки управления
 }
 
-static void  ft_pathfinder(void)
+static void  ft_pathfinder(void)                    // решаем в каком режиме будем работать: Экзамен или Номер Кластера
 {
     unsigned int  sleep_length;
 
     if (rtc_g.exam_state)
-        ft_exam_mode(&sleep_length);
+        ft_exam_mode(&sleep_length);                // режим Экзамен отвечает только за отрезок времени 1 час до начала экзамена и до конца экзамена
     else
-        ft_cluster_number_mode(&sleep_length);
+        ft_cluster_number_mode(&sleep_length);      // за всё остальное время отвечает режим Номер Кластера
     ft_go_to_sleep(sleep_length);
     DEBUG_PRINTF("This message will never be printed out", "");
 }
