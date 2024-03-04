0c17a3586495fa1096e2a439c546843bae816806
RomanAlexandroff <r.aleksandroff@gmail.com>
2024-03-04 17:56:46 +0100

    refactor setup and work logic

--- 42-Prague-Smart-Sign.ino
+++ 42-Prague-Smart-Sign.ino
@@ -1,24 +1,31 @@
 
 #include "42-Prague-Smart-Sign.h"
 
-void  setup(void)
+static void  setup(void)
 {
     #ifdef DEBUG
         Serial.begin(115200);
         ft_delay(50);
         DEBUG_PRINTF("\n\nDEVICE START\nversion %f\n", float(SOFTWARE_VERSION));
     #endif
-    ft_eeprom_init();
-    ft_battery_init();
-    ft_display_init();
-    ft_power_down_recovery();
-    ft_battery_check();
-    ft_buttons_init();
-    ft_wifi_connect();
-    ft_get_time();
-    
-    
-    ft_go_to_sleep(time_of_sleep);
+    ft_eeprom_init();           // всегда
+    ft_battery_init();          // всегда
+    ft_display_init();          // всегда
+    ft_power_down_recovery();   // всегда
+    ft_battery_check();         // всегда
+    ft_buttons_init();          // всегда
+}
+
+static void  ft_pathfinder(void)
+{
+    unsigned int  sleep_length;
+
+    if (rtc_g.exam_state)
+        ft_exam_mode(&sleep_length);
+    else
+        ft_cluster_number_mode(&sleep_length);
+    ft_go_to_sleep(sleep_length);
+    DEBUG_PRINTF("This message will never be printed out", "");
 }
 
 void  loop(void) { /* NOTHING IS HERE */ }
