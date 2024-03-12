6814398b5d33c9f8315bd98193dfd13c2aafac9d
RomanAlexandroff <r.aleksandroff@gmail.com>
2024-03-12 22:54:27 +0100

    calibrate settings for display to use less RAM

--- 42-Prague-Smart-Sign.h
+++ 42-Prague-Smart-Sign.h
@@ -3,29 +3,28 @@
 # define _42_PRAGUE_SMART_SIGN_H
 
 # include <Arduino.h>                     // String variables manipulations
-//# include <esp_wifi.h>
-# include <WiFiUdp.h>
-# include <ESPmDNS.h>
-# include <ArduinoOTA.h>
-# include "EEPROM.h"                      // saves data even without electricity
+# include <WiFiUdp.h>                     // for the Over The Air update functionality
+# include <ESPmDNS.h>                     // for the Over The Air update functionality
+# include <ArduinoOTA.h>                  // for the Over The Air update functionality
+# include "EEPROM.h"                      // stores data even without electricity
 # include <Wire.h>                        // manages SPI connections for the display
 # include <time.h>                        // gets NTP Server date and time
-# include <stdio.h>
+# include <stdio.h>                       // provides printf() function for the DEBUG macro
 # include <stdint.h>                      // provides fixed-width integer types
-# include <esp_system.h>
+# include <esp_system.h>                  // allows to use ESP-IDF native functions
 # include "esp_sleep.h"                   // allows to use touch pad for wake up
 # include <driver/adc.h>                  // measures battery voltage
 # include "bitmap_library.h"
 # include "globals.h"
 
-void       ft_eeprom_init(void);
-void       ft_battery_init(void);
-void       ft_display_init(void);
+void                ft_eeprom_init(void);
+void                ft_battery_init(void);
+void                ft_display_init(void);
 void                ft_power_down_recovery(void);
 short               ft_battery_check(void);
 void                ft_buttons_init(void);
 static void         ft_pathfinder(void);
-static bool         ft_result_chech(const int address, uint16_t result, uint16_t old_max, uint16_t old_min);
+static bool         ft_result_check(const int address, uint16_t result, uint16_t old_max, uint16_t old_min);
 void IRAM_ATTR      ft_battery_state(const int address);
 void IRAM_ATTR      isr_warning(void);
 void IRAM_ATTR      isr_diagnostics(void);
@@ -34,7 +33,7 @@ void IRAM_ATTR      isr_reboot(void);
 void                ft_ota_init(void);
 static void         ft_ota_waiting_loop(void);
 void                ft_go_to_sleep(uint64_t time_in_millis);
-void       ft_delay(uint64_t time_in_millis);
+void                ft_delay(uint64_t time_in_millis);
 void                ft_wifi_connect(void);
 void                ft_get_time(void);
 unsigned int        ft_time_till_event(uint8_t hours, uint8_t minutes);
@@ -46,18 +45,15 @@ void                ft_cluster_number_mode(unsigned int* p_sleep_length);
 static uint8_t      ft_number_of_exams(String* p_server_message);
 static void         ft_clean_data(String server_message);
 static void         ft_get_data_load(const String token, String* p_server_message);
-static void         ft_get_token(const String* p_token);
-void                ft_fetch_exams(void);
+static bool         ft_get_token(const String* p_token);
+bool                ft_fetch_exams(void);
 
-void ft_display_exam_sign(void);
-void ft_display_timer_exact_end_time(void);
-void ft_display_timer(int minutes);
-void   ft_display_animated_text_with_font(String output);
-void   ft_display_bitmap_with_refresh(const unsigned char* output);
-void   ft_display_bitmap(const unsigned char* output);
-void   ft_display_battery_state(void);
-bool   ft_clear_display(bool errase_display);
-
+void                ft_display_exam_sign(void);
+void                ft_display_timer_exact_end_time(void);
+void                ft_display_timer(int minutes);
+void                ft_display_cluster_number(uint8_t mode);
+void                ft_display_battery_state(void);
+bool                ft_clear_display(bool errase_display);
 
 #endif
  
--- globals.h
+++ globals.h
@@ -2,14 +2,14 @@
 #ifndef GLOBALS_H_
 # define GLOBALS_H_
 
-# include <GxEPD2_3C.h>                             // 3-coloured version of the library
-# include <Adafruit_GFX.h>
-# include <Fonts/FreeSansBold24pt7b.h>
+# include "constants.h"
+# include <GxEPD2_BW.h>                             // we still need the black/white library version
+# include <GxEPD2_3C.h>                             // 3-coloured version of the GxEPD2 library
+# include <Fonts/FreeSansBold24pt7b.h>              // the fonts come from the Adafruit GFX library which gets called by the GxEPD2 library
 # include <WiFiClientSecure.h>                      // has to be here. For Wi-Fi connection and HTTPS requests
 # include "credentials.h"
-# include "constants.h"
 
-extern GxEPD2_3C<GxEPD2_750c_Z08, GxEPD2_750c_Z08::HEIGHT> display; // GDEY075Z08 800x480
+extern GxEPD2_3C<GxEPD2_750c_Z08, GxEPD2_750c_Z08::HEIGHT/2> display; // GDEY075Z08 800x480
 extern WiFiClientSecure client;
 
 struct RTC_GlobalsManagement {
@@ -31,7 +31,7 @@ struct RTC_GlobalsManagement {
 extern struct RTC_GlobalsManagement rtc_g;
 
 struct GlobalsManagement {
-    uint8_t battery;
+    uint16_t battery;
 };
 extern struct GlobalsManagement g;
 
