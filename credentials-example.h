2cebc7c678759acb3f5b11b33e4136b98890e65b
RomanAlexandroff <r.aleksandroff@gmail.com>
2024-04-09 14:23:06 +0200

    add example of credentials file

---
+++ credentials-example.h
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   credentials.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raleksan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/09 13:00:46 by raleksan          #+#    #+#             */
+/*   Updated: 2024/04/09 13:00:48 by raleksan         ###   ########.fr       */
+/*                                                                            */
+/*                                                                            */
+/*  ATTENTION! CONFIDENTIAL INFORMATION!                                      */
+/*                                                                            */
+/*  This file contains confidential information,  thus neither the file nor   */
+/*  its content may be copied, published, photographed  or otherwise shared   */
+/*  without absolute necessity.                                               */
+/*                                                                            */
+/*  By copying the file or its content you undertake full responsibility to   */
+/*  ensure its safety and confidentiality. E.g., make sure the file name is   */
+/*  properly  stated in the .gitignore  file so it will not get uploaded to   */
+/*  GitHub. Under  no  circumstances  it  may  leak  to  the  Internet. All   */
+/*  the occurred leaks must be  reported  to  the 42 campus administration.   */
+/*  Please,  educate  yourself  on  how  to  deal  with  confidential files   */
+/*  during software development on the Internet.                              */
+/*                                                                            */
+/*  The SECRET token, the UID and the BOT_TOKEN are for this project purpose  */
+/*  only! They may not be used for any other project.  Not even temporarily!  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CREDENTIALS_H
+# define CREDENTIALS_H
+
+# define BOT_TOKEN       "xxxxxxxxxx:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"       // Telegram Bot
+
+# define SECRET          "x-xxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+# define UID             "x-xxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+# define CAMPUS_ID       "xx"                // your campus number on Intra
+# define CLUSTER_ID      "xx"
+
+# define TIME_ZONE       1                   // your campus time zone according to the GMT standart
+
+# define WIFI_SSID       "your_campus_wifi_name"
+# define WIFI_PASSWORD   "your_campus_wifi_password"
+
+#endif
+ 
