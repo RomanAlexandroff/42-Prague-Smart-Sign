63e91d3c5ed440f7ca193eb60226a154bdee6c8b
RomanAlexandroff <r.aleksandroff@gmail.com>
2024-07-14 12:37:16 +0200

    fix potential infinite reboot because of time obtaining error

--- cluster_number_mode.cpp
+++ cluster_number_mode.cpp
@@ -27,29 +27,38 @@ static unsigned int  ft_time_till_wakeup(void)
 
 void  ft_cluster_number_mode(unsigned int* p_sleep_length)
 {
-    uint8_t i;
-    bool    intra_connected;
-    int8_t  days_left;
+    RTC_DATA_ATTR static uint8_t retries = 0;
+    bool                         intra_connected;
+    int8_t                       days_left;
 
-    i = 0;
     intra_connected = false;
     ft_wifi_connect();
     ft_telegram_check();
     if (!ft_get_time())
     {
-        *p_sleep_length = 60000;
         rtc_g.exam_status = false;
+        if (retries == 4)
+        {
+            retries = 0;
+            ft_display_cluster_number(INTRA_ERROR);
+            *p_sleep_length = ft_time_till_wakeup();
+            DEBUG_PRINTF("ERROR OBTAINING TIME. Cannot proceed. Turning off\n", "");
+            return;
+        }
+        *p_sleep_length = 60000;
+        retries++;
         DEBUG_PRINTF("Retrying in 1 minute\n", "");
         return;
     }
-    while (!intra_connected && i < 2)
+    retries = 0;
+    while (!intra_connected && retries < 2)
     {
-        delay (i * 300000);
-        DEBUG_PRINTF("Fetching exams data — try #%d\n\n", i + 1);
+        delay (retries * 300000);
+        DEBUG_PRINTF("Fetching exams data — try #%d\n\n", retries + 1);
         intra_connected = ft_fetch_exams();
-        i++;
-        if (!intra_connected && i < 3)
-            DEBUG_PRINTF("\nRetrying in %d minutes\n\n", i * 5);
+        retries++;
+        if (!intra_connected && retries < 3)
+            DEBUG_PRINTF("\nRetrying in %d minutes\n\n", retries * 5);
     }
     if (!intra_connected && rtc_g.hour <= 18)
     {
