b806f73cc3b758580fe15b2068ae5af6b19d1b40
RomanAlexandroff <r.aleksandroff@gmail.com>
2024-09-07 10:59:52 +0200

    update comments

--- exam_mode.cpp
+++ exam_mode.cpp
@@ -17,7 +17,7 @@ static unsigned int ft_exam(void)
     unsigned int  exam_remaining_time;
 
     exam_remaining_time = ft_time_till_event(rtc_g.exam_end_hour, rtc_g.exam_end_minutes);
-    ft_draw_colour_bitmap(exam_warning_black, exam_warning_red);
+    ft_draw_colour_bitmap(exam_warning_black, exam_warning_red);                          // execution takes 25 sec
     rtc_g.exam_status = false;
     return (exam_remaining_time);
 }
@@ -29,19 +29,19 @@ static void ft_preexam_warning(unsigned int* p_preexam_time)
     minutes = ft_time_sync(*p_preexam_time);
     if (minutes == 60 || minutes == 50)
     {
-        ft_draw_colour_bitmap(preexam_50mins, preexam_warning_red);                       // execution may take up to 40 sec
+        ft_draw_colour_bitmap(preexam_50mins, preexam_warning_red);                       // execution takes 25 sec
         ft_delay((minutes - 40) * 60000);
         minutes = 40;
     }
     if (minutes == 40 || minutes == 30 || minutes == 20)
     {
-        ft_draw_colour_bitmap(preexam_25mins, preexam_warning_red);                       // execution may take up to 40 sec
+        ft_draw_colour_bitmap(preexam_25mins, preexam_warning_red);                       // execution takes 25 sec
         ft_delay((minutes - 10) * 60000);
         minutes = 10;
     }
     if (minutes == 10)
     {
-        ft_draw_colour_bitmap(preexam_5mins, preexam_warning_red);                        // execution may take up to 40 sec
+        ft_draw_colour_bitmap(preexam_5mins, preexam_warning_red);                        // execution takes 25 sec
         ft_delay(480000);
     }
     *p_preexam_time = 0;
