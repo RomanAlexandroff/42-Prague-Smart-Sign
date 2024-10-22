186e2503259975af6437e33f98e44e42229cbc47
RomanAlexandroff <r.aleksandroff@gmail.com>
2024-10-22 16:12:52 +0200

    refactor cluster mode for readability + add error messages

--- cluster_number_mode.cpp
+++ cluster_number_mode.cpp
@@ -12,48 +12,37 @@
 
 #include "42-Prague-Smart-Sign.h"
 
-static void ft_report_exception(ERROR_t intra_status)
+static void ft_report_exception(ERROR_t status)
 {
-    ft_display_cluster_number(INTRA_ERROR);
-    rtc_g.exam_status = false;
     if (WiFi.status() != WL_CONNECTED)
         ft_wifi_connect();
     if (WiFi.status() != WL_CONNECTED)
         return;
-    if (intra_status == INTRA_NO_SERVER)
-        bot.sendMessage(rtc_g.chat_id, ft_compose_message(INTRA_NO_SERVER, 0), "");
-    else if (intra_status == INTRA_NO_TOKEN)
-        bot.sendMessage(rtc_g.chat_id, ft_compose_message(INTRA_NO_TOKEN, 0), "");
-    else if (intra_status == INTRA_NO_INFO)
-        bot.sendMessage(rtc_g.chat_id, ft_compose_message(INTRA_NO_INFO, 0), "");
+    bot.sendMessage(rtc_g.chat_id, ft_compose_message(status, 0), "");
 }
 
-void  ft_cluster_number_mode(unsigned int* p_sleep_length)
+/* CALCULATE THE SECRET EXPIRATION AND DISPLAY + NOTIFY */
+static bool ft_handle_secret_expiration(void)
+{
+    int8_t  days_left;
+    
+    days_left = ft_expiration_counter();
+    if (days_left <= 3 && days_left != FAILED_TO_COUNT)
+    {
+        ft_display_cluster_number(SECRET_EXPIRED);
+        DEBUG_PRINTF("\n[INTRA] IT IS TIME TO UPDATE THE SECRET!\n", "");
+        bot.sendMessage(rtc_g.chat_id, ft_compose_message(SECRET_EXPIRED, days_left), "");
+        return (true);
+    }
+    return (false);
+}
+
+/* MAKE SURE TO GET EXAMS INFO OR DISPLAY ERROR */
+static bool ft_ensure_exams(unsigned int* p_sleep_length)
 {
     uint8_t retries;
     ERROR_t intra_status = UNKNOWN;
-    int8_t  days_left;
-
-    retries = 0;
-    if (WiFi.status() != WL_CONNECTED)
-        ft_wifi_connect();
-
-    /* MAKE SURE TO HAVE ACTUAL TIME OR TURN OFF FOR 6 HOURS */
-    while (!ft_get_time() && retries < RETRIES_LIMIT)
-    {
-        retries++;
-        if (retries == RETRIES_LIMIT)
-        {
-            ft_display_cluster_number(INTRA_ERROR);
-            DEBUG_PRINTF("\n[SYSTEM TIME] ERROR OBTAINING TIME. Cannot proceed. Turning off\n", "");
-            *p_sleep_length = ft_time_till_wakeup();
-            return;
-        }
-        DEBUG_PRINTF("\n[SYSTEM TIME] Retrying in %d minute(s)\n", retries * 5);
-        delay (retries * 300000);
-    }
-
-    /* MAKE SURE TO GET EXAMS INFO OR DISPLAY ERROR */
+    
     retries = 0;
     while (intra_status != INTRA_OK && retries < RETRIES_LIMIT)
     {
@@ -65,9 +54,51 @@ void  ft_cluster_number_mode(unsigned int* p_sleep_length)
             DEBUG_PRINTF("\n[INTRA] Retrying in %d minute(s)\n", retries * 5);
     }
     if (intra_status != INTRA_OK)
+    {
+        ft_display_cluster_number(INTRA_ERROR);
+        DEBUG_PRINTF("\n[INTRA] ERROR OBTAINING EXAMS. Turning off\n", "");
         ft_report_exception(intra_status);
+        rtc_g.exam_status = false;
+        *p_sleep_length = ft_time_till_wakeup();
+        return (false);
+    }
+    return (true);
+}
 
-    /* CALCULATE HOW LONG TO SLEEP TILL PRE-EXAM ANNOUNCEMENT AND TURN OFF */
+/* MAKE SURE TO HAVE ACTUAL TIME OR TURN OFF FOR 6 HOURS */
+static bool ft_ensure_time(unsigned int* p_sleep_length)
+{
+    uint8_t retries;
+    ERROR_t time_status = UNKNOWN;
+    
+    retries = 0;
+    while (time_status != TIME_OK && retries < RETRIES_LIMIT)
+    {
+        time_status = ft_get_time();
+        retries++;
+        DEBUG_PRINTF("\n[SYSTEM TIME] Retrying in %d minute(s)\n", retries * 5);
+        delay (retries * 300000);
+    }
+    if (time_status != TIME_OK)
+    {
+        ft_display_cluster_number(INTRA_ERROR);
+        DEBUG_PRINTF("\n[SYSTEM TIME] ERROR OBTAINING TIME. Cannot proceed. Turning off\n", "");
+        ft_report_exception(time_status);
+        rtc_g.exam_status = false;
+        *p_sleep_length = ft_time_till_wakeup();
+        return (false);
+    }
+    return (true);
+}
+
+void  ft_cluster_number_mode(unsigned int* p_sleep_length)
+{
+    if (WiFi.status() != WL_CONNECTED)
+        ft_wifi_connect();
+    if (!ft_ensure_time(p_sleep_length))
+        return;
+    if (!ft_ensure_exams(p_sleep_length))
+        return;
     if (rtc_g.exam_status)
     {
         if (com_g.hour >= (rtc_g.exam_start_hour - 1) && com_g.hour <= com_g.exam_end_hour)
@@ -79,25 +110,9 @@ void  ft_cluster_number_mode(unsigned int* p_sleep_length)
         }
         return;
     }
-
-    /* CALCULATE HOW LONG TO SLEEP TILL NEXT WAKE-UP */
-    else
-        *p_sleep_length = ft_time_till_wakeup();
-    
-    /* IF FAILED TO GET EXAMS INFO, TURN OFF HERE TILL NEXT WAKE-UP */
-    if (intra_status != INTRA_OK)
+    *p_sleep_length = ft_time_till_wakeup();
+    if (ft_handle_secret_expiration())
         return;
-    
-    /* CALCULATE THE SECRET EXPIRATION AND DISPLAY + NOTIFY */
-    days_left = ft_expiration_counter();
-    if (days_left <= 3 && days_left != FAILED_TO_COUNT)
-    {
-        ft_display_cluster_number(SECRET_EXPIRED);
-        bot.sendMessage(rtc_g.chat_id, ft_compose_message(SECRET_EXPIRED, days_left), "");
-        return;
-    }
-
-    /* DISPLAY ORDINARY DAY (NO ISSUES, NO FAILS, NO EXAMS) AND TURN OFF TILL NEXT WAKE-UP */
-    ft_display_cluster_number(DEFAULT);
+    ft_display_cluster_number(DEFAULT);    // DISPLAY ORDINARY DAY (NO ISSUES, NO FAILS, NO EXAMS) AND TURN OFF TILL NEXT WAKE-UP
 }
  
