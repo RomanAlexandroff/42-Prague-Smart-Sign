48b22b67da5e8f53c0cfd0d4f859899338b5de3a
RomanAlexandroff <r.aleksandroff@gmail.com>
2024-11-02 13:01:26 +0100

    refactor switch statement for readability

--- display_handling.cpp
+++ display_handling.cpp
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: raleksan <r.aleksandroff@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/09 13:00:56 by raleksan          #+#    #+#             */
-/*   Updated: 2024/08/31 13:00:00 by raleksan         ###   ########.fr       */
+/*   Created: 2024/04/09 13:00:00 by raleksan          #+#    #+#             */
+/*   Updated: 2024/11/02 12:30:00 by raleksan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -160,77 +160,63 @@ void IRAM_ATTR  ft_display_cluster_number(IMAGE_t mode)
         display_cluster = true;
         displaying_now = CLUSTER;
     }
-    switch (mode)
+    if (mode == DEFAULT_IMG && displaying_now != DEFAULT_IMG)
     {
-        case STOCK:
-            if (displaying_now == STOCK)
-                return;
-            ft_draw_bitmap_partial_update(default_cluster_icons, 170, 480);
-            displaying_now = STOCK;
-            break;
-        case INTRA_ERROR:
-            if (displaying_now == INTRA_ERROR)
-                return;
-            ft_draw_bitmap_partial_update(intra_error_img, 170, 480);
-            displaying_now = INTRA_ERROR;
-            break;
-        case SECRET_EXPIRED:
-            if (displaying_now == SECRET_EXPIRED)
-                return;
-            ft_draw_bitmap_partial_update(secret_expire_img, 170, 480);
-            displaying_now = SECRET_EXPIRED;
-            break;
-        case EXAM_DAY:
-            if (displaying_now == EXAM_DAY)
-                return;
-            ft_draw_bitmap_partial_update(reserve_note_img, 170, 480);
-            delay (500);
-            ft_draw_exam_start_time();
-            display_cluster = false;
-            displaying_now = EXAM_DAY;
-            break;
-        case LOW_BATTERY:
-            if (displaying_now == LOW_BATTERY)
-                return;
-            ft_draw_bitmap_partial_update(low_battery_img, 170, 480);
-            displaying_now = LOW_BATTERY;
-            break;
-        case OTA_WAITING:
-            if (displaying_now == OTA_WAITING)
-                return;
-            ft_draw_text("WAITING FOR OTA UPDATE", 50, 710);
-            displaying_now = OTA_WAITING;
-            break;
-        case OTA_UPDATING:
-            if (displaying_now == OTA_UPDATING)
-                return;
-            ft_draw_text("OTA UPDATE IN PROGRESS...", 50, 710);
-            displaying_now = OTA_UPDATING;
-            break;
-        case OTA_SUCCESS:
-            if (displaying_now == OTA_SUCCESS)
-                return;
-            ft_draw_text("OTA UPDATE SUCCESS", 50, 710);
-            displaying_now = OTA_SUCCESS;
-            break;
-        case OTA_FAIL:
-            if (displaying_now == OTA_FAIL)
-                return;
-            ft_draw_text("OTA UPDATE FAIL", 50, 710);
-            displaying_now = OTA_FAIL;
-            break;
-        case OTA_CANCELED:
-            if (displaying_now == OTA_CANCELED)
-                return;
-            ft_draw_text("OTA UPDATE CANCELED", 50, 710);
-            displaying_now = OTA_CANCELED;
-            break;
-        case TELEGRAM_ERROR:
-            if (displaying_now == TELEGRAM_ERROR)
-                return;
-            ft_draw_text("TELEGRAM BOT ERROR", 50, 710);
-            displaying_now = TELEGRAM_ERROR;
-            break;
+        ft_draw_bitmap_partial_update(default_cluster_icons, 170, 480);
+        displaying_now = DEFAULT_IMG;
+    }
+    else if (mode == INTRA_ERROR && displaying_now != INTRA_ERROR)
+    {
+        ft_draw_bitmap_partial_update(intra_error_img, 170, 480);
+        displaying_now = INTRA_ERROR;
+    }
+    else if (mode == SECRET_EXPIRED && displaying_now != SECRET_EXPIRED)
+    {
+        ft_draw_bitmap_partial_update(secret_expire_img, 170, 480);
+        displaying_now = SECRET_EXPIRED;
+    }
+    else if (mode == EXAM_DAY && displaying_now != EXAM_DAY)
+    {
+        ft_draw_bitmap_partial_update(reserve_note_img, 170, 480);
+        delay (500);
+        ft_draw_exam_start_time();
+        display_cluster = false;
+        displaying_now = EXAM_DAY;
+    }
+    else if (mode == LOW_BATTERY && displaying_now != LOW_BATTERY)
+    {
+        ft_draw_bitmap_partial_update(low_battery_img, 170, 480);
+        displaying_now = LOW_BATTERY;
+    }
+    else if (mode == OTA_WAITING && displaying_now != OTA_WAITING)
+    {
+        ft_draw_text("WAITING FOR OTA UPDATE", 50, 710);
+        displaying_now = OTA_WAITING;
+    }
+    else if (mode == OTA_UPDATING && displaying_now != OTA_UPDATING)
+    {
+        ft_draw_text("OTA UPDATE IN PROGRESS...", 50, 710);
+        displaying_now = OTA_UPDATING;
+    }
+    else if (mode == OTA_SUCCESS && displaying_now != OTA_SUCCESS)
+    {
+        ft_draw_text("OTA UPDATE SUCCESS", 50, 710);
+        displaying_now = OTA_SUCCESS;
+    }
+    else if (mode == OTA_FAIL && displaying_now != OTA_FAIL)
+    {
+        ft_draw_text("OTA UPDATE FAIL", 50, 710);
+        displaying_now = OTA_FAIL;
+    }
+    else if (mode == OTA_CANCELED && displaying_now != OTA_CANCELED)
+    {
+        ft_draw_text("OTA UPDATE CANCELED", 50, 710);
+        displaying_now = OTA_CANCELED;
+    }
+    else if (mode == TELEGRAM_ERROR && displaying_now != TELEGRAM_ERROR)
+    {
+        ft_draw_text("TELEGRAM BOT ERROR", 50, 710);
+        displaying_now = TELEGRAM_ERROR;
     }
 }
 
