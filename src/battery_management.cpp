0d6b2200003688a8616f54845d2ee862bf1de32e
RomanAlexandroff <r.aleksandroff@gmail.com>
2024-12-08 12:27:51 +0100

    update to esp32 board v. 3.0.7: ADC config

--- src/battery_management.cpp
+++ src/battery_management.cpp
@@ -6,7 +6,7 @@
 /*   By: raleksan <r.aleksandroff@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 13:00:00 by raleksan          #+#    #+#             */
-/*   Updated: 2024/11/27 13:20:00 by raleksan         ###   ########.fr       */
+/*   Updated: 2024/12/07 13:20:00 by raleksan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ void  ft_battery_check(void)
         samples_count++;
     }
     battery = battery / samples_count;
+    DEBUG_PRINTF("\n[BATTERY] Current battery state: %d\n", battery);
     if (battery >= BATTERY_GOOD)
         return;
     if (WiFi.status() != WL_CONNECTED)
@@ -34,14 +35,14 @@ void  ft_battery_check(void)
     if (battery < BATTERY_CRITICAL)
     {
         ft_display_cluster_number(LOW_BATTERY);
-        DEBUG_PRINTF("\nBattery is too low. Going into extensive sleep\n", "");
+        DEBUG_PRINTF("\n[BATTERY] Battery is too low. Going into extensive sleep\n");
         bot.sendMessage(String(rtc_g.chat_id), ft_compose_message(DEAD_BATTERY, 0), "");
         ft_go_to_sleep(DEAD_BATTERY_SLEEP);
     }
     else if (battery < BATTERY_GOOD)
     {
         ft_display_cluster_number(LOW_BATTERY);
-        DEBUG_PRINTF("\nLow battery! Need charging!\n", "");
+        DEBUG_PRINTF("\n[BATTERY] Low battery! Need charging!\n");
         bot.sendMessage(String(rtc_g.chat_id), ft_compose_message(LOW_BATTERY, 0), "");
     }
 }
@@ -50,6 +51,6 @@ void  ft_battery_init(void)
 {
     ft_watchdog_reset();
     adc1_config_width(ADC_WIDTH_BIT_12);
-    adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_11db);
+    adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_DB_12);
 }
  
